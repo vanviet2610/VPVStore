74e7584213703d11de1f1a60e75fa804e3e32763
vanviet26102 <vanviet2610>
2022-09-10 17:44:47 +0700

    build project

---
+++ app/Http/Controllers/api/UsersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Users;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Models\Users  $users
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Users $users)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Models\Users  $users
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Users $users)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Models\Users  $users
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Users $users)
+    {
+        //
+    }
+}
