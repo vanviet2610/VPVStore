d98bc8fbbf726c562238a990c8810294d78cde60
vanviet2610 <82449147+vanviet2610@users.noreply.github.com>
2022-09-11 18:51:30 +0700

    .

--- resources/views/layouts_master/manager_master.blade.php
+++ resources/views/layouts_master/manager_master.blade.php
@@ -12,83 +12,17 @@
     <link rel="shortcut icon" href="assets\images\favicon.ico">
 
     <!-- App css -->
-    <link href="assets\css\bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet">
-    <link href="assets\css\icons.min.css" rel="stylesheet" type="text/css">q
-    <link href="assets\css\app.min.css" rel="stylesheet" type="text/css" id="app-stylesheet">
+    <link href="{{ asset('manager/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"
+        id="bootstrap-stylesheet">
+    <link href="{{ asset('manager/css/icons.min.css') }}" rel="stylesheet" type="text/css">q
+    <link href="{{ asset('manager/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-stylesheet">
 
 </head>
 
-<body class="authentication-page">
+<body>
 
-    <div class="account-pages my-5">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8 col-lg-6 col-xl-5">
-                    <div class="card mt-4">
-                        <div class="card-header text-center p-4 bg-primary">
-                            <h4 class="text-white mb-0 mt-0">Velonic</h4>
-                            <h5 class="text-white font-13 mb-0">Create a new Account</h5>
-                        </div>
-                        <div class="card-body">
-                            <form action="#" class="p-2">
-
-                                <div class="form-group mb-3">
-                                    <label for="emailaddress">Email :</label>
-                                    <input class="form-control" type="email" id="emailaddress" required="" placeholder="john@deo.com">
-                                </div>
-
-                                <div class="form-group mb-3">
-                                    <label for="username">User name :</label>
-                                    <input class="form-control" type="text" id="username" required="" placeholder="john deo">
-                                </div>
-
-                                <div class="form-group mb-3">
-                                    <label for="password">Password :</label>
-                                    <input class="form-control" type="password" id="password" required="" placeholder="Enter your password">
-                                </div>
-
-                                <div class="form-group mb-4">
-                                    <div class="checkbox checkbox-success">
-                                        <input id="remember" type="checkbox" checked="">
-                                        <label for="remember">
-                                                I accept <strong><a href="#">Terms and Conditions</a></strong>
-                                            </label>
-                                    </div>
-                                </div>
-
-                                <div class="form-group text-right mt-4 mb-4">
-                                    <div class="col-12">
-                                        <button class="btn btn-md btn-primary waves-effect waves-light" type="submit">Register</button>
-                                    </div>
-                                </div>
-
-                                <div class="form-group row mb-0">
-                                    <div class="col-sm-12 text-center">
-                                        <a href="pages-login.html">Already have account?</a>
-                                    </div>
-                                </div>
-                            </form>
-
-                        </div>
-                        <!-- end card-body -->
-                    </div>
-                    <!-- end card -->
-
-                    <!-- end row -->
-
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
-
-        </div>
-    </div>
-
-    <!-- Vendor js -->
-    <script src="assets\js\vendor.min.js"></script>
-
-    <!-- App js -->
-    <script src="assets\js\app.min.js"></script>
+    <script src="{{ asset('manager/js/vendor.min.js') }}"></script>
+    <script src="{{ asset('manager/js/app.min.js') }}"></script>
 
 </body>
 
